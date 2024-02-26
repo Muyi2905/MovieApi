a63798fb4e4d7f316725c2f8b8ba6df9477a9c9b
muyi2905 <obaremimuyiwa@gmail.com>
2024-02-26 15:02:10 +0000

    update

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const MovieRoutes = require("./routes/movie.js");
+const ShowRoutes = require("./routes/show.js");
+const Movie = require("./models/movies");
 
 const PORT = 4000;
 
 const app = express();
 
 app.use(express.json());
+app.use(Movie, MovieRoutes);
+app.use(Show, ShowRoutes);
 
 mongoose
-  .connect("mongodb://localhost:27017/Crud", {
+  .connect("mongodb://localhost:27017/MovieApi", {
     useNewUrlParser: true, // Update if using Mongoose >= 6.0.0
     useUnifiedTopology: true, // Update if using Mongoose >= 6.0.0
   })
