a6a4dadd3fbc658318715741cf501f312b010f5d
muyi2905 <obaremimuyiwa@gmail.com>
2024-02-26 15:01:53 +0000

    movie schema created

---
+++ models/movies.js
@@ -0,0 +1,96 @@
+const mongoose = require("mongoose");
+
+const MovieSchema = new mongoose.Schema({
+  Title: {
+    type: String,
+    required: true,
+  },
+  Genre: {
+    type: [String],
+    required: true,
+  },
+  ReleaseYear: {
+    type: Number,
+    required: true,
+  },
+  Cast: [
+    {
+      actorName: {
+        type: String,
+        required: true,
+      },
+      role: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  Crew: [
+    {
+      crewMemberName: {
+        type: String,
+        required: true,
+      },
+      role: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  Ratings: [
+    {
+      source: {
+        type: String,
+        required: true,
+      },
+      value: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  Reviews: [
+    {
+      reviewer: {
+        type: String,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  Trailers: [
+    {
+      url: {
+        type: String,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  StreamingAvailability: [
+    {
+      platform: {
+        type: String,
+        required: true,
+      },
+      link: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Movie = mongoose.model("Movie", MovieSchema);
+
+module.exports = Movie;
